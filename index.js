67df0c0888b48bc6d243b5f57a2df1489978a1aa
Hang Pham <tpham.hang@gmail.com>
2017-10-08 23:21:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    console.log("I can't hear you!")
+    return "I can't hear you!"
   }
 }
